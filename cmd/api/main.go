57c435f205670134758665aa2adeff6d150b2baf
FASIL <fasilkp2000@gmail.com>
2022-12-29 16:46:56 +0530

    Beg 1.2 : connect the database and return the memmory address

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,15 +5,15 @@ import (
 	"log"
 
 	"github.com/fazilnbr/project-workey/pkg/config"
+	"github.com/fazilnbr/project-workey/pkg/db"
 )
 
 func main() {
 	fmt.Println("starting my job-portal project in clean code architecture")
 
-	fmt.Println("starting my job-portal project in clean code architecture")
 	config, configErr := config.LoadConfig()
 	if configErr != nil {
 		log.Fatal("cannot load config: ", configErr)
 	}
-	fmt.Printf("\n\nconfig : %v\n\n", config)
+	db.ConnectDB(config)
 }
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -1,1 +1,39 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	config "github.com/fazilnbr/project-workey/pkg/config"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB(cfg config.Config) *sql.DB {
+
+	databaseName := os.Getenv("DB_NAME")
+
+	dbURI := cfg.DBSOURCE
+
+	//Opens database
+	db, err := sql.Open("postgres", dbURI)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// verifies connection to the database is still alive
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error in pinging")
+		log.Fatal(err)
+
+	}
+
+	log.Println("\nConnected to database:", databaseName)
+
+	return db
+
+}
