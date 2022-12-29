5a5eb622385c57dd1a5f7b9f253fef448363227e
FASIL <fasilkp2000@gmail.com>
2022-12-29 16:25:12 +0530

    Beg : loding the enviorment variable using viper package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/fazilnbr/project-workey/pkg/config"
+)
 
 func main() {
 	fmt.Println("starting my job-portal project in clean code architecture")
+
+	fmt.Println("starting my job-portal project in clean code architecture")
+	config, configErr := config.LoadConfig()
+	if configErr != nil {
+		log.Fatal("cannot load config: ", configErr)
+	}
+	fmt.Printf("\n\nconfig : %v\n\n", config)
 }
