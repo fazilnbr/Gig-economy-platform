b44b3c7f74659186b755f8751baeecc8f7462096
FASIL <fasilkp2000@gmail.com>
2022-12-30 14:21:48 +0530

    Swag 1.1 : Here auto geneatate the api documentation using swag properly

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"log"
 
+	_ "github.com/fazilnbr/project-workey/cmd/api/docs"
+	_ "github.com/fazilnbr/project-workey/pkg/common/response"
+	_ "github.com/fazilnbr/project-workey/pkg/domain"
+
 	"github.com/fazilnbr/project-workey/pkg/config"
 	"github.com/fazilnbr/project-workey/pkg/db"
 
 	"github.com/fazilnbr/project-workey/pkg/di"
 )
 
+// @title Go + Gin Workey API
+// @version 1.0
+// @description This is a sample server Job Portal server. You can visit the GitHub repository at https://github.com/fazil
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:3000
+// @BasePath /
+// @query.collection.format multi
 func main() {
 	fmt.Println("starting my job-portal project in clean code architecture")
 
@@ -20,7 +38,6 @@ func main() {
 	db.ConnectDB(config)
 	gorm, _ := db.ConnectGormDB(config)
 	fmt.Printf("\ngorm : %v\n\n", gorm)
-
 
 	server, diErr := di.InitializeAPI(config)
 	fmt.Printf("\n\n\nserver ; %v\n\n\n", server)
