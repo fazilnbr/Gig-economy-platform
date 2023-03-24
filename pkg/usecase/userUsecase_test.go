8c86758b32ac621081e55c287ec3a9f826148f63
fazilnbr <fazilkp2000@gmail.com>
2023-03-24 20:46:35 +0530

    mocked userUsecase
    mock the user usecase interface

--- pkg/usecase/userUsecase_test.go
+++ pkg/usecase/userUsecase_test.go
@@ -5,53 +5,53 @@ import (
 	"testing"
 
 	"github.com/fazilnbr/project-workey/pkg/domain"
-	"github.com/fazilnbr/project-workey/pkg/mock"
+	mock "github.com/fazilnbr/project-workey/pkg/mock/repoMock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFindUser(t *testing.T) {
-    ctrl := gomock.NewController(t)
-    defer ctrl.Finish()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-    c := mock.NewMockUserRepository(ctrl)
+	c := mock.NewMockUserRepository(ctrl)
 
-    userusecase := NewUserService(c)
-    testData := []struct {
-        name       string
-        email      string
-        beforeTest func(userRepo *mock.MockUserRepository)
-        expectErr  error
-    }{
-        {
-            name:  "Test success response",
-            email: "jon",
-            beforeTest: func(userRepo *mock.MockUserRepository) {
-                userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{
-                    UserName: "jon",
-                    Password: "12345",
-                }, nil)
-            },
-            expectErr: nil,
-        },
+	userusecase := NewUserService(c)
+	testData := []struct {
+		name       string
+		email      string
+		beforeTest func(userRepo *mock.MockUserRepository)
+		expectErr  error
+	}{
 		{
-            name:  "Test when user alredy exist response",
-            email: "jon",
-            beforeTest: func(userRepo *mock.MockUserRepository) {
-                userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{}, errors.New("repo error"))
-            },
-            expectErr: errors.New("repo error"),
-        },
-    }
+			name:  "Test success response",
+			email: "jon",
+			beforeTest: func(userRepo *mock.MockUserRepository) {
+				userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{
+					UserName: "jon",
+					Password: "12345",
+				}, nil)
+			},
+			expectErr: nil,
+		},
+		{
+			name:  "Test when user alredy exist response",
+			email: "jon",
+			beforeTest: func(userRepo *mock.MockUserRepository) {
+				userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{}, errors.New("repo error"))
+			},
+			expectErr: errors.New("repo error"),
+		},
+	}
 
-    for _, tt := range testData {
-        t.Run(tt.name, func(t *testing.T) {
-            tt.beforeTest(c)
-            actualUser, err := userusecase.FindUser(tt.email)
-            assert.Equal(t, tt.expectErr, err)
-            if err == nil {
-                assert.Equal(t, tt.email, actualUser.UserName)
-            }
-        })
-    }
-}	
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.beforeTest(c)
+			actualUser, err := userusecase.FindUser(tt.email)
+			assert.Equal(t, tt.expectErr, err)
+			if err == nil {
+				assert.Equal(t, tt.email, actualUser.UserName)
+			}
+		})
+	}
+}
