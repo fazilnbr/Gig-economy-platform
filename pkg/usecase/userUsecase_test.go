b8ec7a87e66c2d802ca07e1d812f76906cb59017
fazilnbr <fazilkp2000@gmail.com>
2023-03-25 13:00:45 +0530

    usecase test
    user usecase test done where is two repo calling

--- pkg/usecase/userUsecase_test.go
+++ pkg/usecase/userUsecase_test.go
@@ -55,3 +55,75 @@ func TestFindUser(t *testing.T) {
 		})
 	}
 }
+
+func TestCreateUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	c := mock.NewMockUserRepository(ctrl)
+
+	userusecase := NewUserService(c)
+	testData := []struct {
+		name       string
+		user       domain.User
+		beforeTest func(userRepo *mock.MockUserRepository)
+		expectErr  error
+	}{
+		{
+			name: "Test success response",
+			user: domain.User{
+				UserName: "jon",
+				Password: "12345",
+			},
+			beforeTest: func(userRepo *mock.MockUserRepository) {
+				userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{
+					UserName: "jon",
+					Password: "12345",
+				}, errors.New("find user repo error"))
+				userRepo.EXPECT().InsertUser(domain.User{
+					UserName: "jon",
+					Password: "827ccb0eea8a706c4c34a16891f84e7b",
+					Verification: false,
+				}).Return(1, nil)
+			},
+			expectErr: nil,
+		},
+		{
+			name: "Test user already exist response",
+			user: domain.User{
+				UserName: "jon",
+				Password: "12345",
+			},
+			beforeTest: func(userRepo *mock.MockUserRepository) {
+				userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{}, nil)
+			},
+			expectErr: errors.New("Username already exists"),
+		},
+		{
+			name: "Test insert repo error response",
+			user: domain.User{
+				UserName: "jon",
+				Password: "12345",
+			},
+			beforeTest: func(userRepo *mock.MockUserRepository) {
+				userRepo.EXPECT().FindUser("jon").Return(domain.UserResponse{
+					UserName: "jon",
+					Password: "12345",
+				}, errors.New("find user repo error"))
+				userRepo.EXPECT().InsertUser(domain.User{
+					UserName: "jon",
+					Password: "827ccb0eea8a706c4c34a16891f84e7b",
+				}).Return(1, errors.New("insertuser repo error"))
+			},
+			expectErr: errors.New("insertuser repo error"),
+		},
+	}
+
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.beforeTest(c)
+			 err := userusecase.CreateUser(tt.user)
+			assert.Equal(t, tt.expectErr, err)
+		})
+	}
+}
